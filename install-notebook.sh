7672baf80919503ecfc1dc76b48889b6c2369d5f
Özgür Akgün <ozgurakgun@gmail.com>
2022-06-23 16:36:22 +0100

    Making a copy of the installation script instead
    We could set up a Github action to automatically update this file I suppose...

---
+++ install-notebook.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# remove the sample-data directory that google colab creates, we don't need them
+rm -rf sample_data
+
+# download latest release from Github
+rm -rf conjure-notebook-executable
+git clone --quiet https://github.com/ogabek96/conjure-notebook-executable.git
+
+(
+cd conjure-notebook-executable
+
+# download the latest conjure (temporary location)
+rm conjure
+wget -q https://ozgur.host.cs.st-andrews.ac.uk/files/conjure
+
+# copy the executables to /usr/local/bin
+chmod +x conjure savilerow savilerow.jar solvers/*
+cp -R conjure savilerow savilerow.jar solvers/* /usr/local/bin
+)
+
+# we don't need to keep these around any more
+rm -rf conjure-notebook-executable
+
+# installing the conjure extension
+pip --quiet install git+https://github.com/conjure-cp/conjure-notebook.git
+
+conjure --version
